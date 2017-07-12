0c5adc1bd44c93d068cb57bbcdb4a300250dc30b
Kombucha <4143920@qq.com>
2017-07-12 17:00:57 +1200

    Initial commit

---
+++ src/page/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home_container">
+        <itemcontainer father-component="home"></itemcontainer>
+    </div>
+</template>
+
+<script>
+    import itemcontainer from '../../components/itemcontainer'
+    export default {
+        name: 'home',
+        components: {
+            itemcontainer
+        },
+        created(){
+
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .home_container {
+
+    }
+</style>
---
+++ src/store/action.js
@@ -0,0 +1,14 @@
+/**
+ * Created by timzhao on 12/07/17.
+ */
+export default {
+    addNum({ commit, state }, id){
+        commit('REMBER_ANSWER', id);
+        if (state.itemNum < state.itemDetail.length) {
+            commit('ADD_ITEMNUM',1)
+        }
+    },
+    initializeData({ commit }) {
+        commit('INITIALIZE_DATA')
+    }
+}
